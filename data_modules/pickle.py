7f8ca4abd53c82a56ad844ab199cdd5dea5fc2b6
alegrar <47478054+alegrar@users.noreply.github.com>
2021-07-24 23:47:32 +0900

    Update pickle.py

---
+++ data_modules/pickle.py
@@ -0,0 +1,28 @@
+import glob
+import os
+import pickle as pickle
+
+
+def pickle_examples(with_charid=True):
+    os.makedirs('./pickledata')
+    dir_lst = os.listdir('./train_set')
+    c = -1
+    with open('./pickledata/train.pickle', 'wb') as ft:
+        for font_dir in dir_lst:
+            c += 1
+            final_dir = './train_set/' + font_dir
+            paths = glob.glob(os.path.join(final_dir, "*.png"))
+            print('all data num:', len(paths))
+            if with_charid:
+                print('pickle with charid')
+                for p in paths:
+                    label = c 
+                    _idx = os.path.basename(p).rfind('_') + 1
+                    charid = int(os.path.basename(p)[_idx:].split(".")[0], 16) 
+
+                    with open(p, 'rb') as f:
+                        img_bytes = f.read()
+                        example = (label, charid, img_bytes)
+                        pickle.dump(example, ft)
+
+    return print("We've just pickled pickle!")
